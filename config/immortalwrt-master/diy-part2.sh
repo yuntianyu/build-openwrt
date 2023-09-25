81ddd0fdb84ef5480b513953404e0e8a3d9a3c70
2018nuoyan <42512451+2018nuoyan@users.noreply.github.com>
2023-09-25 19:44:18 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -9,21 +9,21 @@
 # ------------------------------- Main source started -------------------------------
 #
 # Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root:$1$HRLXOZuW$K3ujiT12JkbgC1TIh5Qd30:19478:0:99999:7:::/g' package/base-files/files/etc/shadow
+#sed -i 's/root:$1$HRLXOZuW$K3ujiT12JkbgC1TIh5Qd30:19478:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='nuoyan'" >>package/base-files/files/etc/openwrt_release
+#sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+#echo "DISTRIB_SOURCECODE='nuoyan'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/kiddin9/openwrt-packages package/
+#svn co https://github.com/kiddin9/openwrt-packages package/
 
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
