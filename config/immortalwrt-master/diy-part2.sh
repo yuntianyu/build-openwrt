23f27ba8489e1155454caf6c42ab20e0a34295e9
2018nuoyan <42512451+2018nuoyan@users.noreply.github.com>
2023-09-26 21:07:37 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -12,10 +12,8 @@
 #sed -i 's/root:$1$HRLXOZuW$K3ujiT12JkbgC1TIh5Qd30:19478:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_REVISION='OpenWrt $(date +%Y.%m.%d) by nuoyan'|g" package/base-files/files/etc/openwrt_release
-sed -i "s/OpenWrt /nuoyan build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
-echo "$(date +%Y.%m.%d)" >package/base-files/files/etc/openwrt_version
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 #sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
