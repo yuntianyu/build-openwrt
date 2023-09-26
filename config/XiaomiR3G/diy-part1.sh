9454d62364e5129b0b036e3e6724f0655eb6ba3c
2018nuoyan <1198678747@qq.com>
2023-09-26 23:24:51 +0800

    更新设备信息

---
+++ config/XiaomiR3G/diy-part1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
+#========================================================================================================================
+
+# Add a feed source
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+#sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+#sed -i '1i src-git packages https://github.com/openwrt/packages.git !luci-app-mwan3 !luci-app-syncdial !luci-app-opkg
+
+# other
+# rm -rf package/emortal/{autosamba,ipv6-helper}
+
